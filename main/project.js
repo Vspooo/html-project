b18aa18db28acc8e9d11643ea33e7cdaf94bac03
Vspooo <voloshunns@gmail.com>
2024-02-16 21:31:39 +0200

    first part

---
+++ main/project.js
@@ -0,0 +1,35 @@
+let carousel;
+carousel = document.querySelector(".carousel");
+let arrowButton;
+arrowButton = document.querySelectorAll(".wrap4>i")
+let firstCardWeight;
+firstCardWeight =  carousel.querySelector(".card").offsetWidth
+
+const dragging = (e)=>{
+    if (!isDragging)return;
+    carousel.scrollLeft= startScrollLeft - (e.pageX - startX);
+     carousel.classList.add("dragging");
+}
+
+const dragStart = (e) =>{
+    isDragging = true;
+    startX = e.pageX
+    startScrollLeft =  carousel.scrollLeft
+}
+
+const dragStop = () =>{
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+arrowButton.forEach( button =>{
+    button.addEventListener("click", ()=>{
+        carousel.scrollLeft += button.id === "left" ? -firstCardWeight:firstCardWeight;
+    })
+})
+
+let isDragging = false, startX, startScrollLeft;
+
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mousedown", dragStart);
+document.addEventListener("mouseup", dragStop)
